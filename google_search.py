d4795d0cefb53a7d97eb143be2193fbef7eebfd4
cang <dovancang@gmail.com>
2022-10-16 16:49:18 +0700

    done

--- google_search.py
+++ google_search.py
@@ -3,7 +3,9 @@
 
 # selenium 4
 import os
+import sys
 import json
+import html #html.escape(myHtml)
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -15,7 +17,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def google_search(kw):
-    '''Input is any keyword, the function will return array of dict(title,link)'''
+    '''Input is any keyword, the function will return array of dict(title,link,des)'''
     RESULT_CONTENT_ID = 'rso'
     RESULT_TIMEOUT = 5
     
@@ -51,31 +53,36 @@ def google_search(kw):
       return rets;    
       
     childrens = eparent.find_elements(By.XPATH,"./child::*")
-    print("result item numer {}".format(len(childrens)))
-    
     for erow in childrens:
       item = {}
      
       try:
-        #print(len(erow.find_elements(By.XPATH,"descendant::div/span")))
-        
+        #find title from h3 below link
         eh3 = erow.find_element(By.XPATH,"descendant::a/h3")
         if(eh3):
-          item['title'] = eh3.text
-          if(item['title']):
-            item['title'] = item['title'].encode('utf-8')
+          item['title'] = eh3.text.encode('utf-8')
             
+          #link is h3's parent 
           ea = eh3.find_element(By.XPATH,"..")
           if(ea):
             item['link'] = ea.get_attribute('href')
-
+            
+            #find up 3 level check chilren more than 2 and this is short description :)
+            #adiv = ea.find_element(By.XPATH,"../../..").find_elements(By.XPATH,"./child::*")
+            ea = ea.find_element(By.XPATH,"..")
+            for i in range(2):
+              ea = ea.find_element(By.XPATH,"..")
+              adiv = ea.find_elements(By.XPATH,"./child::*")
+              if(len(adiv)>1):
+                item['des'] = adiv[1].get_attribute("textContent").encode("utf-8")
+                break
+                
         rets.append(item)
             
       except Exception as err:
-        pass
-      finally:
-        pass
-        
+        #Some exception Unable to locate element: {"method":"xpath","selector":"descendant::a/h3"}
+        #print(err) 
+        continue
    
     #time.sleep(10)
     #driver.implicitly_wait(10)
@@ -83,10 +90,14 @@ def google_search(kw):
     
     return rets
 
-rets = google_search('Chargebacks911')
-#print(json.dumps(rets))
+
+# entry  
+args = sys.argv[1:]
+rets = []
+if(len(args)>0):
+  rets = google_search(args[0])
+else:
+  rets = google_search('Chargebacks911')  
+
 for item in rets:
-  print(item)
-
-
-
+  print(item)  
